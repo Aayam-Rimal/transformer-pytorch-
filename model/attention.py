b81fa450704f1debcf8782ee651eebde1a556c66
Aayam Rimal <rimalaayam87@gmail.com>
2026-06-05 21:05:15 +0545

    half implemented multihead attention class

--- model/attention.py
+++ model/attention.py
@@ -29,6 +29,45 @@ class SelfAttention(nn.Module):
         out= torch.matmul(attn,V)
 
         return out 
+    
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self,d_model, head ):
+        super().__init__()
+
+        assert d_model % head == 0
+        
+        self.head= head
+
+        self.head_dim= d_model // head
+
+        self.q_proj= nn.Linear(d_model,d_model)
+        self.k_proj= nn.Linear(d_model,d_model)
+        self.v_proj= nn.Linear(d_model,d_model)
+
+
+    def forward(self,x, mask=None):
+
+        B,S,D= x.shape
+
+        Q= self.q_proj(x)
+        K= self.k_proj(x)
+        V= self.v_proj(x)
+
+        Q= Q.view(B,S,self.head, self.head_dim).permute(0,2,1,3)
+        K= K.view(B,S,self.head, self.head_dim).permute(0,2,1,3)
+        V= V.view(B,S,self.head, self.head_dim).permute(0,2,1,3)
+
+        score= Q @ K.permute(0,1,3,2)
+        score= score/(self.head_dim ** 0.5)
+
+
+
+
+
+
+
 
 
 
