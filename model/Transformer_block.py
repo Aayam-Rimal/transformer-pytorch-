aad8f7dee497348c86e08e787a531b90109cac4f
Aayam Rimal <rimalaayam87@gmail.com>
2026-06-06 15:49:19 +0545

    added feed forward network class

--- model/Transformer_block.py
+++ model/Transformer_block.py
@@ -19,3 +19,16 @@ class LayerNorm(nn.Module):
         x_norm= (x-mean)/torch.sqrt(var + self.eps)
 
         return self.gamma * x_norm + self.beta
+    
+
+class FFN(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super().__init__()
+
+        self.W1= nn.Linear(d_model,d_ff)
+
+        self.W2= nn.Linear(d_ff, d_model)
+
+    def forward(self,x ):
+
+        return self.W2(torch.relu(self.W1(x)))
