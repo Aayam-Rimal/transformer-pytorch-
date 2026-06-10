211d005f3c19be18c709f5cdff88faeccf42725b
Aayam Rimal <rimalaayam87@gmail.com>
2026-06-10 10:58:45 +0545

    wrapped the vocab in a function and cleaned the text

--- Data/vocab.py
+++ Data/vocab.py
@@ -1,50 +1,61 @@
 from datasets import load_dataset
 import json
-
-ds = load_dataset("Helsinki-NLP/opus-100", "en-it")
-
-src_vocab= set()
-tgt_vocab= set()
-
-for sample in ds["train"]:
-    en= sample["translation"]["en"]
-    it= sample["translation"]["it"]
-
-    en_tokens= en.lower().split()
-    it_tokens= it.lower().split()
-
-    src_vocab.update(en_tokens)
-    tgt_vocab.update(it_tokens)
-
-src_id= {
-    "<pad>": 0,
-    "<unk>": 1,
-    "<bos>": 2,
-    "<eos>": 3
-}
-
-tgt_id= {
-    "<pad>": 0,
-    "<unk>": 1,
-    "<bos>": 2,
-    "<eos>": 3
-}
-
-for word in sorted(src_vocab):
-
-    if word not in src_id:
-
-       src_id[word]= len(src_id)
-
-for word in sorted(tgt_vocab):
-
-    if word not in tgt_id:
-
-       tgt_id[word]= len(tgt_id)
+import re
 
 
-with open("src_vocab.json","w") as f:
-    json.dump(src_id,f)
+def clean(text):
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Zà-ÿ\s]", "", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
 
-with open("tgt_vocab.json","w") as f:
-    json.dump(tgt_id,f)
+
+def build_vocab(save_src="src_vocab.json", save_tgt="tgt_vocab.json"):
+
+    ds = load_dataset("Helsinki-NLP/opus-100", "en-it")
+    
+    src_vocab= set()
+    tgt_vocab= set()
+    
+    for sample in ds["train"]:
+        en= clean(sample["translation"]["en"])
+        it= clean(sample["translation"]["it"])
+    
+        src_vocab.update(en.split())
+        tgt_vocab.update(it.split())
+    
+    src_id= {
+        "<pad>": 0,
+        "<unk>": 1,
+        "<bos>": 2,
+        "<eos>": 3
+    }
+    
+    tgt_id= {
+        "<pad>": 0,
+        "<unk>": 1,
+        "<bos>": 2,
+        "<eos>": 3
+    }
+    
+    for word in sorted(src_vocab):
+    
+        if word not in src_id:
+    
+          src_id[word]= len(src_id)
+    
+    for word in sorted(tgt_vocab):
+    
+        if word not in tgt_id:
+    
+          tgt_id[word]= len(tgt_id)
+    
+    
+    with open(save_src,"w") as f:
+        json.dump(src_id,f)
+    
+    with open(save_tgt,"w") as f:
+        json.dump(tgt_id,f)
+
+
+build_vocab()
