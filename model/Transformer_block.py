76dc00c11f27098b8dc7e6999632cffe32c00199
Aayam Rimal <rimalaayam87@gmail.com>
2026-06-12 14:38:40 +0545

    change in how attention module was imported in the file

--- model/Transformer_block.py
+++ model/Transformer_block.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-from attention import MultiHeadAttention,CrossAttention
+from .attention import MultiHeadAttention,CrossAttention
 
 
 class LayerNorm(nn.Module):
