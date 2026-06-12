42bda692604669c7da834484c9f8e884c9c15c4e
Aayam Rimal <rimalaayam87@gmail.com>
2026-06-12 14:37:07 +0545

    added embedding logic and linear layer on top of it

--- model/Transformer.py
+++ model/Transformer.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-from Transformer_block import encoder_block,decoder_block
+from .Transformer_block import encoder_block,decoder_block
 
 class encoder_stack(nn.Module):
 
@@ -36,16 +36,24 @@ class decoder_stack(nn.Module):
 
 class Transformer(nn.Module):
     
-    def __init__(self, encoder, decoder):
+    def __init__(self, encoder, decoder, embed_dim,src_vocab_size,tgt_vocab_size):
         super().__init__()
+
+        self.src_embedding= nn.Embedding(src_vocab_size,embed_dim)
+        self.tgt_embedding= nn.Embedding(tgt_vocab_size,embed_dim)
+
+        self.fc_out= nn.Linear(embed_dim, tgt_vocab_size)
 
         self.encoder= encoder
         self.decoder= decoder 
 
     def forward(self,src,trgt):
 
+        src= self.src_embedding(src)
+        trgt= self.tgt_embedding(trgt)
+
         enc_out= self.encoder(src)
         dec_out= self.decoder(trgt,enc_out)
 
-        return dec_out
+        return self.fc_out(dec_out)
     
